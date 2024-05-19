eac6713044c8d3522d7d217bee9baf246273c24e
MinseoSong <mins041600@naver.com>
2024-05-19 16:07:22 +0900

    main 브랜치 생성

---
+++ app/src/main/java/com/example/diethub/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.diethub
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.diethub.ui.theme.DietHubTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DietHubTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                }
+            }
+        }
+    }
+}
